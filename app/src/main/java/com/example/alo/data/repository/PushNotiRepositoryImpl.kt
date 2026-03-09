fb999db7a8362557d6ef43b16fca61da197c7b12
PQ-Thinh <thinh.pq.64cntt@ntu.edu.vn>
2026-03-09 16:33:47 +0700

    set up save FCM Token

--- app/src/main/java/com/example/alo/data/repository/PushNotiRepositoryImpl.kt
+++ app/src/main/java/com/example/alo/data/repository/PushNotiRepositoryImpl.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import com.example.alo.domain.repository.PushNotiRepository
 import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -13,30 +12,15 @@ import javax.inject.Inject
 class PushNotiRepositoryImpl @Inject constructor() : PushNotiRepository {
 
     override suspend fun getDeviceToken(): String? {
-        return withContext(Dispatchers.IO) { // Ép chạy trên luồng phụ, tránh nghẽn App
-            var token: String? = null
-            var retryCount = 0
-            val maxRetries = 3 // Thử tối đa 3 lần
-
-            while (retryCount < maxRetries) {
-                try {
-                    // Dùng .await() cực kỳ an toàn
-                    token = FirebaseMessaging.getInstance().token.await()
-                    Log.d("FCM_DEBUG", "1. Lấy token Firebase THÀNH CÔNG: $token")
-                    break // Lấy thành công thì thoát vòng lặp ngay
-                } catch (e: Exception) {
-                    retryCount++
-                    Log.e("FCM_DEBUG", "1. LỖI lấy token lần $retryCount: ${e.message}")
-
-                    if (retryCount >= maxRetries) {
-                        Log.e("FCM_DEBUG", "1. Đã thử $maxRetries lần nhưng bó tay.", e)
-                        break
-                    }
-                    // Nghỉ 2 giây cho Google Play "thở" rồi gọi lại
-                    delay(2000L)
-                }
+        return withContext(Dispatchers.IO) {
+            try {
+                val token = FirebaseMessaging.getInstance().token.await()
+                Log.d("FCM_DEBUG", "1. Lấy token Firebase THÀNH CÔNG: $token")
+                token
+            } catch (e: Exception) {
+                Log.e("FCM_DEBUG", "1. LỖI lấy token Firebase: ${e.message}", e)
+                null
             }
-            token
         }
     }
 
